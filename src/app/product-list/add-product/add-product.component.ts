33c9ad7b43655782c3d725a24537b1fae0e23fe5
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-10-01 15:23:18 +0100

    Create the form in Add Product via the dynamic form component

--- src/app/product-list/add-product/add-product.component.ts
+++ src/app/product-list/add-product/add-product.component.ts
@@ -1,60 +1,97 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ICreateForm } from 'src/models/form/create-form';
+import { QuestionBase } from 'src/models/form/question-base';
 import { GetSimpleLocation } from 'src/models/location/get-simple-location.dto';
-import { CreateProductDto } from 'src/models/product/create-product-dto';
+import { IAddProduct } from 'src/models/product/add-product';
 import { LocationService } from 'src/services/location.service';
 import { ProductService } from 'src/services/product.service';
+import { QuestionService } from 'src/services/question.service';
 import { ToastService } from 'src/services/toast.service';
 
 @Component({
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss']
+  styleUrls: ['./add-product.component.scss'],
 })
 export class AddProductComponent implements OnInit {
+  questions: Observable<QuestionBase<any>[]>;
 
-  createForm = new FormGroup({
-    name: new FormControl(null),
-    description: new FormControl(null),
-    quantity: new FormControl(null),
-    unit: new FormControl(null),
-    locationId: new FormControl(null),
-    formula: new FormControl(null),
-    expirationDate: new FormControl()
-  });
-
-  selectedLocation: number = 0;
-  locations: GetSimpleLocation[] = [];
+  questionsTypes = [
+    {
+      key: 'name',
+      label: 'Name',
+      type: 'text',
+      order: 1,
+    },
+    {
+      key: 'quantity',
+      label: 'Quantity',
+      type: 'text',
+      order: 2,
+    },
+    {
+      key: 'unit',
+      label: 'Unit',
+      type: 'text',
+      order: 3,
+    },
+    {
+      key: 'formula',
+      label: 'Formula',
+      type: 'text',
+      order: 4,
+    },
+    {
+      key: 'sublocationId',
+      label: 'Sub Location',
+      type: 'dropdown',
+      options: [],
+      order: 5,
+    } as ICreateForm,
+    {
+      key: 'description',
+      label: 'Description',
+      type: 'textarea',
+      order: 6,
+    },
+    {
+      key: 'expirationDate',
+      label: 'Expiration Date',
+      type: 'date',
+      order: 7,
+    },
+  ];
 
   isLoading = false;
 
   constructor(
     private productService: ProductService,
     private locationService: LocationService,
+    private questionService: QuestionService,
     private toastService: ToastService,
-    private router: Router) { }
+    private router: Router
+  ) {
+    this.questions = this.questionService.getQuestions(this.questionsTypes);
+  }
 
   ngOnInit(): void {
     this.locationService
       .getLocations()
-      .subscribe((locations: GetSimpleLocation[]) => this.locations = locations);
+      .subscribe((locations: GetSimpleLocation[]) => {
+        let options = locations.map((location) => ({
+          key: location.id?.toString()!,
+          value: location.name,
+        }));
+
+        this.questionsTypes[4].options = options;
+      });
   }
 
-  selectLocation(e: any) {
-    this.selectedLocation = e.target.value;
-  }
-
-  onSubmit() {
+  onSubmit(payload: any) {
     this.isLoading = true;
-    let product = this.createForm.value as CreateProductDto;
 
-    product.locationId = null;
-
-    if (this.selectedLocation != 0) {
-      product.locationId = this.selectedLocation;
-    }
-
-    this.productService.create(product).subscribe(
+    this.productService.create(payload as IAddProduct).subscribe(
       (res: any) => {
         this.router.navigate(['/products']).then(() => {
           this.toastService.show(res.body, 'Success', false);
@@ -69,14 +106,14 @@ export class AddProductComponent implements OnInit {
           keys.forEach((key: any) => {
             errors[key].forEach((errorMessage: string) => {
               message = errorMessage;
-            })
+            });
           });
 
           this.toastService.show(message, 'Error', true);
           this.isLoading = false;
         }
       },
-      () => this.isLoading = false
+      () => (this.isLoading = false)
     );
   }
 }
--- src/models/product/create-product-dto.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateProductDto {
-    name: string;
-    description: string;
-    locationId: number | null;
-    unit: string;
-    quantity: number;
-    formula: string;
-    expirationDate: Date;
-}
