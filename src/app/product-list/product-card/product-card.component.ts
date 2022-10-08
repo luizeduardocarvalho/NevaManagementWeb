b9eaabdd2131f1be817b455067c72a0193c57850
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-10-08 14:17:58 +0100

    Redesign product card page

--- src/app/product-list/product-card/product-card.component.ts
+++ src/app/product-list/product-card/product-card.component.ts
@@ -21,6 +21,7 @@ export class ProductCardComponent implements OnInit {
   user?: IUser;
 
   isLoading = false;
+  isCardLoading = false;
 
   constructor(
     private productService: ProductService,
