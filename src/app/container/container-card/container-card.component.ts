b54f0c8a53c65217e2ffbf2b10241766b13af0d7
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-10-08 12:45:47 +0100

    Redesign container card page

--- src/app/container/container-card/container-card.component.ts
+++ src/app/container/container-card/container-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faAngleLeft, faReceipt } from '@fortawesome/free-solid-svg-icons';
+import { faReceipt } from '@fortawesome/free-solid-svg-icons';
 import { GetDetailedContainer } from 'src/models/container/get-detailed-container.dto';
 import { GetSimpleContainer } from 'src/models/container/get-simple-container.dto';
 import { ContainerService } from 'src/services/container.service';
@@ -9,10 +9,9 @@ import { ContainerService } from 'src/services/container.service';
 @Component({
   selector: 'app-container-card',
   templateUrl: './container-card.component.html',
-  styleUrls: ['./container-card.component.scss']
+  styleUrls: ['./container-card.component.scss'],
 })
 export class ContainerCardComponent implements OnInit {
-
   faReceipt = faReceipt as IconProp;
 
   container?: GetDetailedContainer;
@@ -21,32 +20,32 @@ export class ContainerCardComponent implements OnInit {
 
   searchText = '';
   isLoading = false;
+  isCardLoading = false;
 
   constructor(
-    private containerService: ContainerService, 
-    private route: ActivatedRoute) { }
+    private containerService: ContainerService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.containerId = params['id'];
+
+      this.isLoading = true;
+      this.containerService
+        .getChildrenContainers(this.containerId)
+        .subscribe((containers: GetSimpleContainer[]) => {
+          this.childrenContainers = containers;
+          this.isLoading = false;
+        });
+
+      this.isCardLoading = true;
+      this.containerService
+        .getDetailedContainer(this.containerId)
+        .subscribe((container: GetDetailedContainer) => {
+          this.container = container;
+          this.isCardLoading = false;
+        });
     });
-    
-    this.isLoading = true;
-
-    this.containerService
-      .getChildrenContainers(this.containerId)
-      .subscribe((containers: GetSimpleContainer[]) => {
-        this.childrenContainers = containers;
-        
-        this.containerService
-          .getDetailedContainer(this.containerId)
-          .subscribe((container: GetDetailedContainer) => {
-            this.container = container;
-
-            this.isLoading = false;
-          }
-        );
-      }
-    );
   }
 }
