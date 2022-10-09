9d77654512b6763089f0e26190c11ce859e59f56
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-10-09 11:58:12 +0100

    Remove menu and header dropdown in the login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NevaManagementWeb';
+  isLoggedIn = true;
 
-  constructor(library: FaIconLibrary) {
+  constructor(
+    library: FaIconLibrary,
+    private router: Router,
+    private authService: AuthService
+  ) {
     library.addIcons(faUser);
+  }
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      if (this.authService.getToken() == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+      }
+    });
+  }
+
+  onLogout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 }
--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faList, faReceipt, faBars } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faHome,
+  faList,
+  faReceipt,
+} from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
-
   faHome = faHome as IconProp;
   faList = faList as IconProp;
   faReceipt = faReceipt as IconProp;
   faBars = faBars as IconProp;
 
-  constructor() { }
+  isLoggedIn = true;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      if (this.authService.getToken() == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+      }
+    });
   }
-
 }
