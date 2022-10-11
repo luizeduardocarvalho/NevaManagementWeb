b075c3e3aaaa0c6e7562734b66416d33019dbb45
Luiz Carvalho <luiz.ferreira.carvalho@hotmail.com>
2022-10-11 22:30:52 +0100

    Fix add quantity page

--- src/app/product-list/add-quantity/add-quantity.component.ts
+++ src/app/product-list/add-quantity/add-quantity.component.ts
@@ -1,39 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { QuestionBase } from 'src/models/form/question-base';
 import { IAddQuantity } from 'src/models/product/add-quantity';
 import { IGetDetailedProduct } from 'src/models/product/get-detailed-product.dto';
 import { ProductService } from 'src/services/product.service';
 import { QuestionService } from 'src/services/question.service';
-import { ToastService } from 'src/services/toast.service';
 
 @Component({
   templateUrl: './add-quantity.component.html',
   styleUrls: ['./add-quantity.component.scss'],
 })
 export class AddQuantityComponent implements OnInit {
-  questions$: QuestionBase<any>[];
+  questions: QuestionBase<any>[];
 
   questionsTypes = [
-    {
-      key: 'name',
-      label: 'Name',
-      type: 'text',
-      order: 1,
-      value: '',
-    },
     {
       key: 'quantity',
       label: 'Quantity',
       type: 'text',
-      order: 2,
+      order: 1,
     },
     {
       key: 'unit',
       label: 'Unit',
       type: 'text',
-      order: 3,
+      order: 2,
       value: '',
+      disabled: true,
     },
   ];
 
@@ -43,11 +37,11 @@ export class AddQuantityComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
-    private toastService: ToastService,
+    private toastr: ToastrService,
     private questionService: QuestionService,
     private router: Router
   ) {
-    this.questions$ = this.questionService.getQuestions(this.questionsTypes);
+    this.questions = this.questionService.getQuestions(this.questionsTypes);
   }
 
   ngOnInit(): void {
@@ -61,39 +55,25 @@ export class AddQuantityComponent implements OnInit {
       .getDetailedProductById(this.productId)
       .subscribe((product: IGetDetailedProduct) => {
         this.isLoading = false;
-        this.questionsTypes[2].value = product.unit;
-        this.questions$ = this.questionService.getQuestions(
-          this.questionsTypes
-        );
+        this.questionsTypes[1].value = product.unit;
+        this.questions = this.questionService.getQuestions(this.questionsTypes);
       });
   }
 
   onSubmit(payload: any) {
     this.isLoading = true;
 
-    this.productService.addProductQuantity(payload as IAddQuantity).subscribe(
+    let parsedPayload = JSON.parse(payload) as IAddQuantity;
+    parsedPayload.productId = this.productId;
+
+    this.productService.addProductQuantity(parsedPayload).subscribe(
       (res: any) => {
+        this.isLoading = false;
         this.router.navigate(['/products/' + this.productId]).then(() => {
-          this.toastService.show(res.body, 'Success', false);
+          this.toastr.success(res.body, 'Success');
         });
       },
-      (err: any) => {
-        let message = '';
-        let errors = err.error.errors;
-
-        if (errors != null) {
-          let keys = Object.keys(errors);
-          keys.forEach((key: any) => {
-            errors[key].forEach((errorMessage: string) => {
-              message = errorMessage;
-            });
-          });
-
-          this.toastService.show(message, 'Error', true);
-          this.isLoading = false;
-        }
-      },
-      () => (this.isLoading = false)
+      (err: any) => (this.isLoading = false)
     );
   }
 }
